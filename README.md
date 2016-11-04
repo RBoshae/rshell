9c443f59be9069ff9d1c91a38e2c13760dcbc8d6
Wanderrer <rwboshae@gmail.com>
2016-11-04 14:15:39 -0700

    working

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # rshell
 CS100 Project, Assignment 2
 
-TODO:
-	Parser,
-	Or class,
-	Container class (Possibly not needed),
-	Ignore comment functionality,
+Introduction: 
+In this assignment we will emulate a shell terminal and call it rshell. When rshell is executed it will accept arguments from the user and execute the argument’s command(s). In order organize and accept multiple commands from the user we will build a Command class. The Command class is designed to be used as an interface. It will also interface with other classes, namely a Parse class, to differentiate multiple arguments and evaluate them properly. The Command class will be built with a Strategy pattern in mind, that is, we will utilize a common interface.
 
+
+BUGS AND TODO LIST:
+
+
+TODO:
+	fix quotation marks
+	add parentathesis
 
 Complete:
 	Argument class,
 	Statement class,
-	Arguemnt_List class,
-	And class
+	And class,
+	Parser,
+	Or class,
+	Container class (Possibly not needed),
+	Ignore comment functionality
 
 Bug Report:
 	And class:
 	execution returns an int to verify if we should run next child. If we have time we may want write a status function to do the same thing.
+	With quotation marks the 
+	no white space after the semi collen infinite loop initiated.
--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,12 @@
 #include "And.h"
 #include "Or.h"
 #include <algorithm> 
-//#include <boost/algorithm/string.hpp> //boost library
 
 using namespace std;
 
-void info()
+void userInfo()
 {
+	//get the users name
   register struct passwd *p;
   register uid_t uid;
   uid = geteuid ();
@@ -33,7 +33,7 @@ void info()
       cout << p->pw_name;
     }
 
-  //retrieve host name
+  //get the users hostname
   char host[500];
   host[499] = '\0';
   gethostname(host, 499);
@@ -72,26 +72,27 @@ int main()
 
 	cout << "**********Welcome to rshell**********" << endl;
 
-	while (continue_shell)
+	while(continue_shell)
 	{
 
 		//Prompt User
 		cout << "$ ";
-		info();
+		userInfo();
 		
 		getline(cin, user_input);//getline will allow us to record and print the string in one line.
 		if(user_input == "exit") break;
 		
 		comment_pos = user_input.find(comment_delimeter);
-		
-		//cout << "comment_pos: " << comment_pos << endl;
-
+		//check for comments
 		if (comment_pos >= 0)
 		{
 			user_input.erase(comment_pos, user_input.size());
 		}
+		
 		while (user_input != "")
 		{
+			//each pos is set to -1 to begin with. Any of the below calls change the pos int, then
+			//you know that a delim of that type exists in the string
 			or_pos = user_input.find(or_delimeter);
 			and_pos = user_input.find(and_delimeter);
 			arg_pos = user_input.find(arg_delimeter);
@@ -102,13 +103,18 @@ int main()
 			if ((or_pos == -1) && (arg_pos == -1) && (and_pos == -1))
 			{
 				st = new Statement(user_input.substr(0, user_input.size()));
+				
+				exiter_pos = user_input.find(exiter);
+				if (exiter_pos >= 0) exit(0);
+				
 				global_truth = st->execute();
 				
 				user_input.erase(0, user_input.size());
+				//cout << "user_input string: "<< user_input << endl;///////////////////////
 			
 			}
 			else
-			{//has delimeters
+			{	//has delimeters
 				//if find pos returns npos set it to 255
 				if (or_pos == -1) or_pos = 2550;
 				if (and_pos == -1) and_pos = 2550;
@@ -119,13 +125,35 @@ int main()
 				
 				command = user_input.substr(0, min_pos);
 				user_input = user_input.substr(min_pos, user_input.size());
+				
 				st = new Statement(command);
 				global_truth = st->execute();
+				//ontinue;////////
 				
 				if (user_input.at(0) == '&')
 				{
 					user_input.erase(0,3);
-					command = user_input.substr(0, min_pos - 1);
+					//cout << "NEW NEW user_input string: "<< user_input << endl;///////////////////////
+					
+					//INJECTION
+					or_pos = user_input.find(or_delimeter);
+					and_pos = user_input.find(and_delimeter);
+					arg_pos = user_input.find(arg_delimeter);
+					
+					if ((or_pos == -1) && (arg_pos == -1) && (and_pos == -1)) min_pos =user_input.size();
+					
+					if (or_pos == -1) or_pos = 2550;
+					if (and_pos == -1) and_pos = 2550;
+					if (arg_pos == -1) arg_pos = 2550;
+					
+					min_pos = min(or_pos, and_pos);
+					min_pos = min(min_pos, arg_pos);
+			
+					///////////////////////////////////////////////
+					
+					command = user_input.substr(0, min_pos - 1);//<<<<<<
+					//cout << "min_pos: " << min_pos << endl;
+					//cout << "NEW command string: "<< command << endl;///////////////////////
 					exiter_pos = command.find(exiter);
 					if (exiter_pos >= 0) exit(0);
 					st = new Statement(command);
@@ -139,6 +167,24 @@ int main()
 				if (user_input.at(0) == '|')
 				{
 					user_input.erase(0,3);
+					
+					//INJECTION
+					or_pos = user_input.find(or_delimeter);
+					and_pos = user_input.find(and_delimeter);
+					arg_pos = user_input.find(arg_delimeter);
+					
+					if ((or_pos == -1) && (arg_pos == -1) && (and_pos == -1)) min_pos =user_input.size();
+					
+					if (or_pos == -1) or_pos = 2550;
+					if (and_pos == -1) and_pos = 2550;
+					if (arg_pos == -1) arg_pos = 2550;
+					
+					min_pos = min(or_pos, and_pos);
+					min_pos = min(min_pos, arg_pos);
+			
+					///////////////////////////////////////////////
+					
+					
 					command = user_input.substr(0, min_pos - 1);
 					
 					exiter_pos = command.find(exiter);
